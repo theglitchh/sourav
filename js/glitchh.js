df777a659ade9f807eb0afd97c4f7d0e65f69107
theglitchh <souravgope765@gmail.com>
2023-07-23 01:43:55 +0530

    blog: add new blogpost

---
+++ js/glitchh.js
@@ -0,0 +1,26 @@
+const createglitchh = () => {
+    let glitchh  = document.querySelector('body');
+    glitchh.innerHTML= `<h1> <span class='gradient'>Coming Back To Reality</span> </h1>
+
+    <p class="about-topic">I F'ed up. </p>
+    <br>
+    <p class="about-topic">I am currently at such a point of my life that i could take any decision in my life
+    and i tried to take a bizzare decision which almost scraped a good chunk of my self esteem, almost made my parents doubt
+    my intellectual knowledge, while stepping into 2023 i simply got a fluke of having a super releveant belief on my luck that 
+    whatever i feel i want i can get it, I agree it wasn't mature of me to do such a big mess in my life.
+    There is no point of revising anything cause this post is meant for my friends or the people i have lived these past 4 years.
+    Everybody was kind to me, but i dont think i was able to be that kind to some people. But life has taught me a lesson now
+    that  <span class="realname" > things we want is far off from the things we actually need </span>. And I guess i tried to get a bit greedy with myself on this matter,
+    that I got over confident on my nature and my way of life and i had a misconception that i can get anything or anyone(mostly) i want.
+    Though that was far away from the reality but then I had mostly failures on this part of my life so I had been a bit greedy or i would say I was being evil.
+    But the thing is I have tried well but then i feel i wanted it and not needed it, some people might stay offshore from me for this but then i guess
+    I have been questioning my thought process just like they have been questioning my nature now. I took a big step not caring about the words of my wellwishers and in return
+    life trashed me back to the ground. Don't know whether this apology is enough(probably not) but to all of my friends and batchmates whoever is reading things
+    I am sorry if I've hurted you because of my foolish/reckless nature but I have taken a lesson out of this and I would say all of you are now moved on to a good 
+    place and are happy or trying to be happy in your life. These 4 years were really good for me and I am happy that i got you guys as my batchmates
+    stay happy and blessed. I have learned from my mistakes and I have grown out of it.   </p>
+    <p class="about-topic"> Signoff: </p>
+    
+    <p class="about-topic">
+    Sourav | TheGlitchh @ 2023 </p> `; }
+    createglitchh();
