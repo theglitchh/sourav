faa80af73c32b237f14e88e0d95c2df7141250e7
theglitchh <souravgope765@gmail.com>
2023-08-20 18:14:32 +0530

    blog: add new blogpost

---
+++ js/sourav.js
@@ -0,0 +1,31 @@
+const createsourav = () => {
+    let sourav  = document.querySelector('body');
+    sourav.innerHTML= `<h1> <span class='gradient'>AT WAR.</span> </h1>
+
+    <p class="about-topic">The Beginning </p>
+    <br>
+    <p class="about-topic">Got all the necessary issues in order. Now I can say I have settled well in Hyderabad, Though I dearly 
+    miss my family
+    it's been a couple of rough weeks going through most of the things mentally, while going through this turmoil of leaving everbody
+    thousands of mile away I feel I have stepped into a different section of the questionnaire of life, from here on I won't have my father
+    to sit down with me and tell me that everything's going to be okay, it would be my responsibility to fixup my own mess whichever i will be making
+    in the near future, though I am paranoid and afraid about the path of life that I am walking right now, but then its the high time for me to 
+    step up and learn about the difficulties that life is about to throw at me, But I have always collectively used people's experiences
+    to avoid certain dead ends, so far my intellectuality has never disappointed me apart of some speed bumps that i have went through as an experiment.
+    But with all that I am still a newbiew..but Engineering has been a great ally to me..the good thing about being a Computer Sceince Engineer
+    is that you get to dwell in the art of logics and wisdoms that each and every semester of it holds, ever since the 2nd year of my Btech
+    I have closely monitored each and every aspects of the subjects and applying them in real life to solve issues, binary tree
+    has to be my most closest topic of solving issues..being a developer or an enthusiast of CODING since the dark days of 2017
+    I have evolved myself to see things a bit differently..evolution of vision and acceptace of mistakes and sufferings has done wonders
+    for me..but it can only take me upto this point of life..from here the scenario is different or should I say the sandbox is different
+    though I have had a bad habit of giving philosphies a name , be it TheFallnn or TheGlitchh or many others that turned to dust 
+    as the evolution of my mind carried on..I feel its a wonderful time for me to not just stick with one philosophy of tau and look a bit inwardly
+    hence I have started my quest deeply into the vedas which i feel i can channel to fruity-fy my computer science knowledge.
+    The words of my ancestor "Krishna" helped Arjuna to win the greatest and most mightiest war. I hope the vedas and The Bhagvat Gita 
+    will also provide an edge to me as I am about to be facing the most cruel war, "The War of Life". As my God Level Developer friend(Sunnyraj) once said
+    "at the end what we do is, git commit -f ", guess I will be doing some "git merge" these days, TheGlitchh is not anymore a sentiment, now it's just a username. </p>
+    <p class="about-topic"> Signoff: </p>
+    
+    <p class="about-topic">
+    Sourav | TheGlitchh @ 2023 </p> `; }
+    createsourav();
